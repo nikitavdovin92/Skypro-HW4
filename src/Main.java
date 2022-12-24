97e6703393732cf71fdd98078e8abd9681394d51
Homework3 <tradervdovin@gmail.com>
2022-12-24 20:09:42 +0500

    Решение до задачи 4 включительно

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,48 @@ public class Main {
         System.out.println("Значение переменной e с типом float равно 1.2f");
         System.out.println("Значение переменной f с типом double равно 2.1");
 
+        float g = 27.12f;
+        long h = 987678965549L;
+        float i = 2.786f;
+        short j = 569;
+        short k = -159;
+        short l = 27897;
+        byte m = 67;
+        System.out.println("Переменная g равна " + g);
+        System.out.println("Переменная h равна " + h);
+        System.out.println("Переменная i равна " + i);
+        System.out.println("Переменная j равна " + j);
+        System.out.println("Переменная k равна " + k);
+        System.out.println("Переменная l равна " + l);
+        System.out.println("Переменная m равна " + m);
+
+        byte lyudmilaPavlovna = 23;
+        short annaSergeevna = 27;
+        int ekaterinaAndreevna = 30;
+        short paper = 480;
+        int pupils = lyudmilaPavlovna + annaSergeevna + ekaterinaAndreevna;
+        int papersForStudent = paper / pupils;
+        System.out.println("На каждого ученика рассчитано " + papersForStudent + " листов бумаги");
+
+        short machinePerformanceInTwoMinutes = 16;
+        int machinePerformanceInTwentyMinutes = machinePerformanceInTwoMinutes * 10;
+        int machinePerformanceInOneDay = machinePerformanceInTwentyMinutes * 72;
+        int machinePerformanceInThreeDays = machinePerformanceInOneDay * 3;
+        float averageDaysPerMonth = 30.4167f;
+        double machineProducedInMonth = machinePerformanceInOneDay * averageDaysPerMonth;
+        System.out.println("За 20 минут машина произвела " + machinePerformanceInTwentyMinutes + " бутылок");
+        System.out.println("За сутки машина произвела " + machinePerformanceInOneDay + " бутылок");
+        System.out.println("За три дня машина произвела " + machinePerformanceInThreeDays + " бутылок");
+        System.out.println("Машина произвела за месяц " + machineProducedInMonth + " бутылок");
+
+
+
+
+
+
+
+
+
 
     }
 
