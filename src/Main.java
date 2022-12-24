6cfbe7358f1138b021a32655e22318fd5a47fb3e
Homework3 <tradervdovin@gmail.com>
2022-12-24 19:09:04 +0500

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        byte b = 2;
+        short c = 3;
+        long d = 4L;
+        float e = 1.2f;
+        double f = 2.1;
+        System.out.println("Значение переменной a с типом int равно 1");
+        System.out.println("Значение переменной b с типом byte равно 2");
+        System.out.println("Значение переменной c с типом short равно 3");
+        System.out.println("Значение переменной d с типом long равно 4L");
+        System.out.println("Значение переменной e с типом float равно 1.2f");
+        System.out.println("Значение переменной f с типом double равно 2.1");
+
+
+    }
+
+
+
+}
